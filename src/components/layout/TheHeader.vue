452659ca4e7588b80d1bba1f115fe96d78c66d36
ShubhamM1203 <96812228+ShubhamM1203@users.noreply.github.com>
2022-07-15 18:34:42 +0530

    Create TheHeader.vue

---
+++ src/components/layout/TheHeader.vue
@@ -0,0 +1,14 @@
+<template>
+    <div id="header">
+    </div>
+</template>
+
+<style scoped>
+#header {
+    border-bottom: solid 1px #ccc;
+    position: fixed;
+    left: 280px;
+    width: 100%;
+    height: 60px;
+}
+</style>
