6ca90a8a8cc98c0d35a0955b3f160493d1287d81
ShubhamM1203 <96812228+ShubhamM1203@users.noreply.github.com>
2022-07-15 18:29:02 +0530

    Add files via upload

---
+++ src/router.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import Blog from "@/pages/blogs/Blog";
+import BlogList from "@/pages/blogs/BlogList";
+import NewBlog from "@/pages/blogs/NewBlog";
+import Highlight from "@/pages/highlights/Highlight";
+import HighlightList from "@/pages/highlights/HighlightList";
+
+import NotFound from "@/pages/NotFound";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", redirect: "/blogs" },
+    { path: "/blogs", component: BlogList },
+    { path: "/blogs/:id", component: Blog, props: true },
+    { path: "/new-blog", component: NewBlog },
+    { path: "/update-blog/:id", component: NewBlog, props: true },
+    {
+      path: "/highlights",
+      component: HighlightList,
+      children: [
+        { path: "/highlights/:id", component: Highlight, props: true },
+      ],
+    },
+    { path: "/:notFound(.*)", component: NotFound },
+  ],
+});
+
+export default router;
