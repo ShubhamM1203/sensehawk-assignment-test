ecd27615d3dfa3e3d5e43d25a944fa267fa64085
ShubhamM1203 <96812228+ShubhamM1203@users.noreply.github.com>
2022-07-15 18:40:28 +0530

    Add files via upload

---
+++ src/pages/highlights/HighlightList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div id="highlightlist">
+    <router-link class="page-title" to="/highlights">HIGHLIGHTS</router-link>
+    <ul v-if="hasHighlights">
+      <li
+        class="highlights--list__item"
+        v-for="highlight in highlights"
+        :key="highlight.id"
+      >
+        <router-link :to="`/highlights/${highlight.id}`">
+          {{ highlight.value }}
+        </router-link>
+      </li>
+    </ul>
+    <h3 v-else>No highlights Found</h3>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    highlights() {
+      return this.$store.getters["highlights"];
+    },
+    hasHighlights() {
+      return this.$store.getters["hasHighlights"];
+    },
+  },
+};
+</script>
+
+<style scoped>
+#blogs-list {
+  display: flex;
+  flex-direction: row;
+}
+
+.highlights--list__item p {
+  margin: 10px 0px 10px 0px;
+}
+
+.highlights--list__item .router-link-active {
+  background-color: yellow;
+}
+</style>
