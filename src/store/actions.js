908e018cbf26dea994f3376d89ea314f08098256
ShubhamM1203 <96812228+ShubhamM1203@users.noreply.github.com>
2022-07-15 18:46:17 +0530

    Add files via upload

---
+++ src/store/actions.js
@@ -0,0 +1,18 @@
+export default {
+  createOrUpdate(context, blog) {
+    context.commit("createOrUpdate", blog);
+  },
+  deleteBlog(context, id) {
+    context.commit("deleteBlog", id);
+  },
+  openClosePopup(context, payload) {
+    context.commit("openClosePopup", payload);
+  },
+  addHighlight(context) {
+    context.commit("addHighlight");
+    context.commit("addHighlightedWordToBlog");    
+  },
+  setSelectedWord(context, payload) {
+    context.commit("setSelectedWord", payload);
+  },
+};
